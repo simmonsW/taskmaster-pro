b20964653beb1ef7bae7cf1a13b146d82fc06a62
Wyatt Simmons <simmonsw182@gmail.com>
2021-03-16 18:54:18 -0700

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -157,15 +157,21 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event) {
+    $(this).addClass('dropover');
+    $('.bottom-trash').addClass('bottom-trash-drag');
     console.log("activate", this);
   },
   deactivate: function(event) {
+    $(this).removeClass('dropover');
+    $('.bottom-trash').removeClass('bottom-trash-drag');
     console.log("deactivate", this);
   },
   over: function(event) {
+    $(event.target).addClass('dropover-active');
     console.log("over", event.target);
   },
   out: function(event) {
+    $(event.target).removeClass('dropover-active');
     console.log("out", event.target);
   },
   update: function(event) {
@@ -208,13 +214,16 @@ $('#trash').droppable({
   accept: '.card .list-group-item',
   tolerance: 'touch',
   drop: function(event, ui) {
+    $('.bottom-trash').removeClass('bottom-trash-active');
     ui.draggable.remove();
     console.log('drop');
   },
   over: function(event, ui) {
+    $('.bottom-trash').addClass('bottom-trash-active');
     console.log('over');
   },
   out: function(event, ui) {
+    $('.bottom-trash').removeClass('bottom-trash-active');
     console.log('out');
   }
 });
@@ -258,7 +267,7 @@ $('#modalDueDate').datepicker({
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -291,4 +300,8 @@ $("#remove-tasks").on("click", function() {
 // load tasks for the first time
 loadTasks();
 
-
+setInterval(function() {
+  $('.card .list-group-item').each(function(index, el) {
+    auditTask(el);
+  });
+}, (1000 * 60) * 30);
